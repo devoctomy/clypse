18d3cbb07e1870ff655c999433be1cb97441996f
Nick Pateman <nick@npsoftware.co.uk>
2025-09-08 11:58:06 +0100

    chore: password generator functional

--- clypse.portal/Components/PasswordGeneratorDialog.razor.cs
+++ clypse.portal/Components/PasswordGeneratorDialog.razor.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Components;
+using clypse.core.Password;
+using clypse.core.Enums;
+using clypse.portal.Models;
 
 namespace clypse.portal.Components;
 
@@ -7,11 +10,116 @@ public partial class PasswordGeneratorDialog : ComponentBase
     [Parameter] public bool Show { get; set; }
     [Parameter] public EventCallback<string> OnPasswordGenerated { get; set; }
     [Parameter] public EventCallback OnCancel { get; set; }
+    [Inject] public IPasswordGeneratorService PasswordGeneratorService { get; set; } = default!;
+    [Inject] public AppSettings AppSettings { get; set; } = default!;
+
+    protected enum PasswordType
+    {
+        Memorable,
+        Random
+    }
+
+    // UI State
+    protected string generatedPassword = string.Empty;
+    protected PasswordType selectedPasswordType = PasswordType.Memorable;
+    
+    // Memorable Password Options
+    protected List<string>? memorablePasswordTemplates;
+    protected string selectedTemplate = string.Empty;
+    protected bool shuffleTokens = false;
+    
+    // Random Password Options
+    protected int passwordLength = 16;
+    protected bool includeLowercase = true;
+    protected bool includeUppercase = true;
+    protected bool includeDigits = true;
+    protected bool includeSpecial = true;
+
+    protected override void OnParametersSet()
+    {
+        if (Show && memorablePasswordTemplates == null)
+        {
+            // Initialize templates from AppSettings
+            memorablePasswordTemplates = AppSettings.MemorablePasswordTemplates?.ToList() ?? new List<string>();
+            
+            // Set default template if available
+            if (memorablePasswordTemplates.Count > 0)
+            {
+                selectedTemplate = memorablePasswordTemplates[0];
+            }
+            
+            // Generate initial password
+            GeneratePassword();
+        }
+    }
+
+    protected void SetPasswordType(PasswordType passwordType)
+    {
+        selectedPasswordType = passwordType;
+        GeneratePassword();
+        StateHasChanged();
+    }
+
+    protected void OnTemplateChanged()
+    {
+        GeneratePassword();
+    }
+
+    protected void OnOptionsChanged()
+    {
+        GeneratePassword();
+    }
+
+    protected void RegeneratePassword()
+    {
+        GeneratePassword();
+    }
+
+    private void GeneratePassword()
+    {
+        try
+        {
+            if (selectedPasswordType == PasswordType.Memorable)
+            {
+                if (!string.IsNullOrEmpty(selectedTemplate))
+                {
+                    generatedPassword = PasswordGeneratorService.GenerateMemorablePassword(selectedTemplate, shuffleTokens);
+                }
+                else
+                {
+                    generatedPassword = "No template selected";
+                }
+            }
+            else // Random
+            {
+                var characterGroups = CharacterGroup.None;
+                
+                if (includeLowercase) characterGroups |= CharacterGroup.Lowercase;
+                if (includeUppercase) characterGroups |= CharacterGroup.Uppercase;
+                if (includeDigits) characterGroups |= CharacterGroup.Digits;
+                if (includeSpecial) characterGroups |= CharacterGroup.Special;
+
+                if (characterGroups == CharacterGroup.None)
+                {
+                    generatedPassword = "Please select at least one character type";
+                }
+                else
+                {
+                    generatedPassword = PasswordGeneratorService.GenerateRandomPassword(characterGroups, passwordLength, atLeastOneOfEachGroup);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            generatedPassword = $"Error: {ex.Message}";
+        }
+
+        StateHasChanged();
+    }
 
     private async Task HandleOk()
     {
-        // Return "Foobar" as requested
-        await OnPasswordGenerated.InvokeAsync("Foobar");
+        await OnPasswordGenerated.InvokeAsync(generatedPassword);
     }
 
     private async Task HandleCancel()
--- clypse.portal/Models/AppSettings.cs
+++ clypse.portal/Models/AppSettings.cs
@@ -4,4 +4,5 @@ public class AppSettings
 {
     public string ApplicationTitle { get; set; } = "Clypse Portal";
     public string CopyrightMessage { get; set; } = "© 2024 Clypse Portal. All rights reserved.";
+    public List<string> MemorablePasswordTemplates { get; set; } = new();
 }
