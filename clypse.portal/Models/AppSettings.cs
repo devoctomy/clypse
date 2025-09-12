c88dcca2a6d00435fdd3e8488efef6c0fd5e3986
Nick Pateman <nick@npsoftware.co.uk>
2025-09-12 14:37:24 +0100

    chore: title bar logo

--- clypse.portal/Models/AppSettings.cs
+++ clypse.portal/Models/AppSettings.cs
@@ -4,5 +4,6 @@ public class AppSettings
 {
     public string ApplicationTitle { get; set; } = "Clypse Portal";
     public string CopyrightMessage { get; set; } = "© 2024 Clypse Portal. All rights reserved.";
+    public bool ShowLogoInTitleBar { get; set; } = false;
     public List<MemorablePasswordTemplateItem> MemorablePasswordTemplates { get; set; } = new();
 }
