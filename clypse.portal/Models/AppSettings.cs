0c74ea3b775e211c7451bbf6cbd099117809a425
Nick Pateman <nick@npsoftware.co.uk>
2025-09-14 15:53:27 +0100

    feat: app version under copyright message on login page

--- clypse.portal/Models/AppSettings.cs
+++ clypse.portal/Models/AppSettings.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 namespace clypse.portal.Models;
 
 public class AppSettings
@@ -6,4 +8,6 @@ public class AppSettings
     public string CopyrightMessage { get; set; } = "© 2024 Clypse Portal. All rights reserved.";
     public bool ShowLogoInTitleBar { get; set; } = false;
     public List<MemorablePasswordTemplateItem> MemorablePasswordTemplates { get; set; } = new();
+    
+    public string Version => Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown";
 }
