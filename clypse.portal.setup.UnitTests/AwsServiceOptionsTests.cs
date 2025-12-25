48ca3ec04694baa625657848444f58a58708c38a
Nick Pateman <nick@npsoftware.co.uk>
2025-12-25 12:40:40 +0000

    chore: test coverage

---
+++ clypse.portal.setup.UnitTests/AwsServiceOptionsTests.cs
@@ -0,0 +1,36 @@
+﻿namespace clypse.portal.setup.UnitTests;
+
+public class AwsServiceOptionsTests
+{
+    [Theory]
+    [InlineData("base-url", "access-id", "secret-access-key", "region", "resource-prefix", true)]
+    [InlineData("", "access-id", "secret-access-key", "region", "resource-prefix", true)]
+    [InlineData("", "", "secret-access-key", "region", "resource-prefix", false)]
+    [InlineData("", "access-id", "", "region", "resource-prefix", false)]
+    [InlineData("", "access-id", "secret-access-key", "", "resource-prefix", false)]
+    [InlineData("", "access-id", "secret-access-key", "region", "", false)]
+    public void GivenValidOptions_WhenIsValid_ThenReturnsTrue(
+        string baseUrl,
+        string accessId,
+        string secretAccessKey,
+        string region,
+        string resourcePrefix,
+        bool expectedIsValid)
+    {
+        // Arrange
+        var sut = new AwsServiceOptions
+        {
+            BaseUrl = baseUrl,
+            AccessId = accessId,
+            SecretAccessKey = secretAccessKey,
+            Region = region,
+            ResourcePrefix = resourcePrefix
+        };
+
+        // Act
+        var isValid = sut.IsValid();
+
+        // Assert
+        Assert.Equal(expectedIsValid, isValid);
+    }
+}
