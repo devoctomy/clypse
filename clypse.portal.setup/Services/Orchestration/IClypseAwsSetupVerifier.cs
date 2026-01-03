abf3e7f7e59aa2ceae39d9a257af3da32aa7a5e6
Nick Pateman <nick@npsoftware.co.uk>
2026-01-03 09:55:59 +0000

    chore: unwanted code

--- clypse.portal.setup/Services/Orchestration/IClypseAwsSetupVerifier.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace clypse.portal.setup.Services.Orchestration;
-
-public interface IClypseAwsSetupVerifier
-{
-    public Task VerifyClypseOnAwsAsync(CancellationToken cancellationToken);
-}
