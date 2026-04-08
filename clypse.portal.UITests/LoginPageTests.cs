9a9f54f79a55199a51677f379b9e284764013a2b
Nick Pateman <nick@npsoftware.co.uk>
2026-04-08 16:53:10 +0100

    chore: tweak

--- clypse.portal.UITests/LoginPageTests.cs
+++ clypse.portal.UITests/LoginPageTests.cs
@@ -38,8 +38,7 @@ public class LoginPageTests : TestBase
         await Expect(Page.Locator(".card-body")).ToBeVisibleAsync();
 
         // Check for version number in the footer
-        var versionType = typeof(Services.BrowserInteropService);
-        var expectedVersion = versionType.Assembly.GetName().Version?.ToString() ?? "Unknown";
+        var expectedVersion = typeof(Services.BrowserInteropService).Assembly.GetName().Version!.ToString();
         await Expect(Page.Locator(".version-number")).ToBeVisibleAsync();
         var allVersionText = (await Page.Locator(".version-number").AllInnerTextsAsync()).ToList();
         Assert.IsTrue(allVersionText.Any(text => text.Contains(expectedVersion)), $"Expected version number '{expectedVersion}' to be displayed in the footer");
